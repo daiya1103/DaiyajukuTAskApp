3b1644a88d2bc8c68d37c2a9d45f732027258056
daiya1103 <doraemonyeah666@gmail.com>
2020-07-07 19:55:47 +0900

    basis-task

---
+++ app/controllers/tasks_controller.rb
@@ -0,0 +1,34 @@
+class TasksController < ApplicationController
+  def index
+      @tasks = Task.all
+  end
+
+  def new
+    @task = current_user.tasks.build
+  end
+
+  def create
+    @task = current_user.tasks.build(task_params)
+    if @task.save
+      redirect_to task_path(@task), notice: '今日もお疲れ様でした^^'
+    else
+      flash_now[:error] = 'まだ書いてない項目があるよ'
+      render :new
+    end
+  end
+
+  def edit
+
+  end
+
+  private
+    def task_params
+        params.require(:article).permit(
+          :exihibits,
+          :solds,
+          :sale,
+          :profit,
+          :impress
+        )
+    end
+end
