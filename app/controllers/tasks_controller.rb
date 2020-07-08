d91d20b8ec5ff99a8587281d34667d177568343d
daiya1103 <doraemonyeah666@gmail.com>
2020-07-08 18:47:16 +0900

    edit-button&delete-button

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -22,7 +22,23 @@ class TasksController < ApplicationController
   end
 
   def edit
+    @task = current_user.tasks.find(params[:id])
+  end
 
+  def update
+    @task = current_user.tasks.find(params[:id])
+    if @task.update(task_params)
+      redirect_to task_path(@task), notice: '更新できました，'
+    else
+      flash.now[:error] = '更新できませんでした'
+      render :edit
+    end
+  end
+
+  def destroy
+    task = current_user.tasks.find(params[:id])
+    task.destroy!
+    redirect_to root_path, notice: '記事を削除しました，'
   end
 
   private
